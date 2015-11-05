4460dd00cb8bc1a8fb86c1375a87ae78cbc36a00
Chancey Ward <chancey_ward@yahoo.com>
2015-11-05 16:04:57 -0600

    made new function for current time

--- public/app.js
+++ public/app.js
@@ -1,3 +1,12 @@
 (function() {
 
+  var startTime = new Date();
+  console.log('First load', startTime);
+
+  function time() {
+    var currentTime = new Date();
+    console.log(currentTime);
+  }
+
+  window.setInterval(time, 1000);
 })();
