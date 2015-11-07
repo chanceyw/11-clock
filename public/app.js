c39c98d2dfc6a7ac941603c9c21594156a91267d
Chancey Ward <chancey_ward@yahoo.com>
2015-11-06 19:22:56 -0600

    added console.logs for testing color generator

--- public/app.js
+++ public/app.js
@@ -1,8 +1,10 @@
 (function() {
   var clock = document.querySelector('.clock');
+  var progress = document.querySelector('.progress');
   var startTime = new Date();
   console.log('First load', startTime);
   window.setInterval(time, 1000);
+
   function time() {
     var currentTime = new Date();
     console.log(currentTime);
@@ -11,6 +13,18 @@
     var nowHour = padZeros(currentTime.getHours());
 
     clock.innerHTML = `${nowHour}:${nowMin}:${nowSec}`;
+
+    console.log(nowSec / 60);
+    progress.style.width = nowSec / 60 * 25 + '%';
+
+    var secColor = colorSpan(nowSec, 60);
+    var minColor = colorSpan(nowMin, 60);
+    var hourColor = colorSpan(nowHour, 60);
+    console.log(secColor);
+    console.log(minColor);
+    console.log(hourColor);
+    debugger;
+
   }
 
   function padZeros(time) {        //if >10 add 0 in front of value.
@@ -21,9 +35,11 @@
     return time;
   }
 
+  var colorSpan = (digit, span) => {
+    return Math.floor(digit / span * 255);
+  };
 
-
+})();
 
   // console.assert(padZeros(1) == '01');
   // console.assert(padZeros(30) == '30');
-})();
